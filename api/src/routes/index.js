8a8adbee46840ddf5ac9baccc15228cab2058ab0
Florencia Ferrari <ferrariflorenciab@gmail.com>
2021-08-04 13:13:04 -0300

    backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,26 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require("axios");
+const { API_KEY } = process.env; // ESTA BIEN ESTO???
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+const getApiInfo = async () => {
+    const apiUrl = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`);
+    const apiInfo = await apiUrl.data.map(e => {
+        return {
+            name: e.title,
+            image: e.image,
+            diets: e.diets.map(e => e),
+            summary: e.summary,
+            healthScore: e.healthScore,
+            rating: e.spoonacularScore
+        }
+    });
+    return apiInfo;
+}
 
 module.exports = router;
