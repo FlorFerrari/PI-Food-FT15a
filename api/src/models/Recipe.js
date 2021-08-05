407a6384c62d2f0f428f646d408d72b92131f7fb
Florencia Ferrari <ferrariflorenciab@gmail.com>
2021-08-05 09:56:09 -0300

    backend

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
           allowNull: false, //campo obligatorio
           primaryKey: true
     },
-    name: {
+    title: {
       type: DataTypes.STRING,
       allowNull: false,
     },
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,11 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const axios = require("axios");
+
+/* const Recipe = require('../models/Recipe');
+const Diet = require('../models/Diet');  */
+const { Recipe, Diet, recipe_diet} = require("../db.js");
+
 const { API_KEY } = process.env; // ESTA BIEN ESTO???
 
 const router = Router();
@@ -10,17 +15,59 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 const getApiInfo = async () => {
     const apiUrl = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`);
-    const apiInfo = await apiUrl.data.map(e => {
+    const apiInfo = await apiUrl.data.results.map(e => {
         return {
-            name: e.title,
-            image: e.image,
-            diets: e.diets.map(e => e),
+         title: e.title, 
+          image: e.image, 
+           diets: e.diets.map(e => e),
             summary: e.summary,
             healthScore: e.healthScore,
-            rating: e.spoonacularScore
-        }
+            rating: e.spoonacularScore 
+        } 
     });
     return apiInfo;
 }
+
+const getDbInfo = async () => {
+    return await Recipe.findAll({
+        include:{
+            model: Diet,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            },
+        }
+    })
+}
+
+const getAllRecipes = async () => {
+    const apiInfo = await getApiInfo();
+    const dbInfo = await getDbInfo();
+    const infoTotal = apiInfo.concat(dbInfo);
+    return infoTotal;
+}
+
+router.get("/recipes", async (req, res) => {
+   /*  const apiUrl = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}&addRecipeInformation=true`);
+    res.status(200).send(apiUrl.data.results.map(e => {return {title: e.title, 
+        image: e.image, 
+         diets: e.diets.map(e => e),
+          summary: e.summary,
+          healthScore: e.healthScore,
+          rating: e.spoonacularScore }})) */
+   
+    
+
+     const { title } = req.query;
+    let recipesTotal = await getAllRecipes();
+    if(title){
+        let recipeName = await recipesTotal.filter( e => e.title.toLowerCase().includes(title.toLowerCase()))
+        recipeName.length ? 
+        res.status(200).send(recipeName) :
+        res.status(404).send("Recipe not found")
+    } else{
+        res.status(200).send(recipesTotal)
+    } 
+})
 
 module.exports = router;
